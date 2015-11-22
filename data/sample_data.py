17f50193eaad0f2d2a195d0a5987e78ec741b4fb
Maury Quijada <maury.quijada@gmail.com>
2015-11-21 19:51:22 -0800

    Move files to data/ folder.

--- data/sample_data.py
+++ data/sample_data.py
@@ -7,22 +7,23 @@ start_time = time.clock()
 full_dataset_conn = sqlite3.connect('database.sqlite')
 full_dataset_curs = full_dataset_conn.cursor()
 
-controversial_dataset_conn = sqlite3.connect('uncontroversial_comments.sqlite')
-controversial_dataset_curs = controversial_dataset_conn.cursor()
+sample_dataset_conn = sqlite3.connect('sample_comments.sqlite')
+sample_dataset_curs = sample_dataset_conn.cursor()
 
 fields = ','.join('?' for _ in range(22))
 
 num = 0
-for row in full_dataset_curs.execute('SELECT * FROM May2015 WHERE controversiality=0 AND random() % 5 = 0 LIMIT 50000'):
+# Change this query according to how you want to sample the data.
+for row in full_dataset_curs.execute('SELECT * FROM May2015 WHERE 1=1'):
   num += 1
   if num % 10000 == 0:
     print "Finished processing %d comments" % num
 
-  controversial_dataset_curs.execute("INSERT INTO May2015 VALUES (%s)" % fields, row)
-  controversial_dataset_conn.commit()
+  sample_dataset_curs.execute("INSERT INTO May2015 VALUES (%s)" % fields, row)
+  sample_dataset_conn.commit()
 
 full_dataset_conn.close()
-controversial_dataset_conn.close()
+sample_dataset_conn.close()
 
 elapsed_time = time.clock() - start_time
 
